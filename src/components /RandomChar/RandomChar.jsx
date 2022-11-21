54b09245705a3aa3c8351d72eedaa5b4532b7c81
HappyKyrgyzNomad <kanatbeks.son@gmail.com>
2022-11-22 00:36:54 +0600

    Changed

--- src/components /RandomChar/RandomChar.jsx
+++ src/components /RandomChar/RandomChar.jsx
@@ -9,30 +9,53 @@ class RandomChar extends Component {
     this.updateChar();
   }
   state = {
-    name: null,
-    description: null,
-    thumbnail: null,
-    homepage: null,
-    wiki: null,
+    char: {},
   };
 
   marvelService = new MarvelService();
 
+  onCharLoaded = (char) => {
+    this.setState({ char });
+  };
+
   updateChar = () => {
-    const id = Math.floor(Math.random() * (1011400 - 1011134) + 1011334);
-    this.marvelService.getCharacter(id).then((res) => {
-      this.setState(res);
-    });
+    const id = Math.floor(Math.random() * (1011334 - 1011400) + 1011334);
+    this.marvelService.getCharacter(id).then(this.onCharLoaded);
   };
 
   render() {
-    const { name, description, thumbnail, homepage, wiki } = this.state;
+    const {
+      char: { name, description, thumbnail, homepage, wiki },
+    } = this.state;
+
+    function refreshPage() {
+      window.location.reload(true);
+    }
+
+    if (this.state.char.name == null) {
+      return (
+        <p className="error">
+          Character not found.{" "}
+          <button className="thorBtn refresh" onClick={refreshPage}>
+            Refresh
+          </button>{" "}
+          page to update data
+        </p>
+      );
+    }
+
+    const descriptionError = () => {
+      return <p>Description not found</p>;
+    };
+
     return (
-      <div className="thor">
+      <div className="thor ">
         <img className="thorImg" src={thumbnail}></img>
         <div>
           <h1 className="charName">{name}</h1>
-          <p className="charDescription">{description}</p>
+          <p className="charDescription error1">
+            {description === "" ? descriptionError() : description}
+          </p>
           <a href={homepage} className="thorBtn">
             Homepage
           </a>
@@ -44,4 +67,5 @@ class RandomChar extends Component {
     );
   }
 }
+
 export default RandomChar;
